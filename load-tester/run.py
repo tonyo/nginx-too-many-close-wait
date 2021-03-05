4a77b728edcaee4408785de940e04981662b601b
Anton Ovchinnikov <anton@tonyo.info>
2021-03-05 11:56:19 +0100

    Simplify load-tester

--- load-tester/run.py
+++ load-tester/run.py
@@ -9,17 +9,17 @@ URL = os.environ.get("TEST_URL", "http://127.0.0.1:8090")
 
 logging.basicConfig(level=logging.DEBUG)
 
+# Non-empty body
 event = {
     "key_{}".format(x): "value_{}".format(x) for x in range(20)
 }
 
-def send_few(x):
+def send_request():
     s = requests.Session()
-    res = s.post(URL, data=event)
-    _ = res.text
+    s.post(URL, data=event)
     s.close()
 
 
 if __name__ == "__main__":
-    with Pool(20) as p:
-        p.map(send_few, range(10000))
+    for _ in range(10000):
+        send_request()
